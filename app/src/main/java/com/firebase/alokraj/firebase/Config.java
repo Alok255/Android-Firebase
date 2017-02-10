ac7f2e2216a960dbd376d2505dbfea54b0888957
Alok Raj Singh <alokrajsingh255@gmail.com>
2017-02-09 23:01:35 -0800

    Firebase

---
+++ app/src/main/java/com/firebase/alokraj/firebase/Config.java
@@ -0,0 +1,9 @@
+package com.firebase.alokraj.firebase;
+
+/**
+ * Created by AlokRaj on 2/8/2017.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://fir-data-3cc39.firebaseio.com/";
+}
---
+++ app/src/main/java/com/firebase/alokraj/firebase/Person.java
@@ -0,0 +1,32 @@
+package com.firebase.alokraj.firebase;
+
+/**
+ * Created by AlokRaj on 2/8/2017.
+ */
+
+public class Person {
+
+    //name and address string
+    private String name;
+    private String address;
+
+    public Person() {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
